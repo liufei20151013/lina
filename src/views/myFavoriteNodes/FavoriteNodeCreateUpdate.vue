3650404325d435712b70ceaf4c17e2c3ea8c958c
liufei20151013 <2199216370@qq.com>
2024-08-19 11:44:38 +0800

    feat: 自定义收藏夹

---
+++ src/views/myFavoriteNodes/FavoriteNodeCreateUpdate.vue
@@ -0,0 +1,27 @@
+<template>
+  <GenericCreateUpdatePage :fields="fields" :initial="initial" :url="url" />
+</template>
+
+<script>
+import GenericCreateUpdatePage from '@/layout/components/GenericCreateUpdatePage'
+
+export default {
+  name: 'FavoriteNodeCreateUpdate',
+  components: {
+    GenericCreateUpdatePage
+  },
+  data() {
+    return {
+      initial: {},
+      fields: [
+        [this.$t('common.Basic'), ['name']]
+      ],
+      url: '/api/v1/assets/favorite-nodes/'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
